e2a8326ee243db3b7e8bb24fd43cd6fbb33e5b97
Vikash Pareek <vikash.pareek1509@gmail.com>
2023-10-14 18:51:54 +0530

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -54,7 +54,7 @@ exports.create = (req, res) => {
     // 1mb = 1000000
 
     if (files.photo) {
-      // console.log("FILES PHOTO: ", files.photo);
+      console.log("FILES PHOTO: ", files.photo);
       if (files.photo.size > 1000000) {
         return res.status(400).json({
           error: 'Image should be less than 1mb in size',
